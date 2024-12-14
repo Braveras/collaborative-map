4f2f27ec1df530d148cd09d7b3772b61144f1ba9
Braveras <acunamatata1997@gmail.com>
2024-12-14 03:00:51 +0100

    Added tiles to improve zoom

--- map.js
+++ map.js
@@ -1,19 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const map = L.map('map').setView([0, 0], 3);
+        const extents = [
+            [0, 0, 6656, 6656],   // Zoom level 0
+            [0, 0, 12288, 12288], // Zoom level 1
+            [0, 0, 24064, 24064]  // Zoom level 2
+        ];
 
-    L.tileLayer('organized-tiles/{z}_{x}_{y}.png', {
-        minZoom: 3,
-        maxZoom: 3,
-        tileSize: 256,
-        attribution: 'Corepunk map test',
-		noWrap: true
-    }).addTo(map);
+        const tileSize = 512;
 
-    const southWest = map.unproject([0, 2048], map.getMaxZoom());
-    const northEast = map.unproject([2048, 0], map.getMaxZoom());
-    const bounds = L.latLngBounds(southWest, northEast);
+        const map = L.map('map', {
+            crs: L.CRS.Simple,
+            minZoom: 0,
+            maxZoom: 2,
+            zoom: 0,
+            center: [3328, 3328],
+            attributionControl: false
+        });
 
-    map.setMaxBounds(bounds);
-    map.fitBounds(bounds);
+        L.TileLayer.CustomTiles = L.TileLayer.extend({
+            getTileUrl: function(coords) {
+                const z = coords.z;
+                const x = coords.x;
+                const y = this.flipY(coords.y, z);
+                return `organized-tiles/${z}/${x}_${y}.png`;
+            },
+            
+            flipY: function(y, z) {
+                const maxTiles = Math.ceil(extents[z][3] / tileSize) - 1;
+                return maxTiles - y;
+            },
+
+            getTileSize: function() {
+                return L.point(tileSize, tileSize);
+            }
+        });
+
+        L.tileLayer.customTiles = function() {
+            return new L.TileLayer.CustomTiles();
+        };
+
+        L.tileLayer.customTiles().addTo(map);
+
+        function updateMapBounds() {
+            const zoom = map.getZoom();
+            const extent = extents[zoom];
+            const southWest = map.unproject([extent[0], extent[3]], zoom);
+            const northEast = map.unproject([extent[2], extent[1]], zoom);
+            map.setMaxBounds(L.latLngBounds(southWest, northEast));
+        }
+
+        map.on('zoomend', updateMapBounds);
+        updateMapBounds();
+
+        map.on('mousemove', function(e) {
+            const point = map.project(e.latlng, map.getZoom());
+            document.getElementById('coordinates').innerHTML = `X: ${Math.floor(point.x)}, Y: ${Math.floor(extents[map.getZoom()][3] - point.y)}`;
+        });
+
+        // Ajustar la vista inicial
+        const initialExtent = extents[0];
+        const southWest = map.unproject([initialExtent[0], initialExtent[3]], 0);
+        const northEast = map.unproject([initialExtent[2], initialExtent[1]], 0);
+        map.fitBounds(L.latLngBounds(southWest, northEast));
 });
-
